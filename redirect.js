6b4e67b7586547872e0c082d37867a8d24358d18
iChizer0 <62390647+iChizer0@users.noreply.github.com>
2021-09-11 02:04:28 +0800

    Typo fixes

--- redirect.js
+++ redirect.js
@@ -2,8 +2,8 @@ function checkServer(msecs, promise) {
     const timeout = new Promise((resolve, reject) => {
         setTimeout(() => {
             reject(new Error('timeout'))
-        }, msecs);
-    });
+        }, msecs)
+    })
     return Promise.race([timeout, promise])
 }
 
