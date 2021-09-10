e3d5eb7b510a50db525b80102ee298357a299e89
iChizer0 <62390647+iChizer0@users.noreply.github.com>
2021-09-11 01:10:37 +0800

    Fix async order error

--- redirect.js
+++ redirect.js
@@ -11,20 +11,30 @@ const testURL = [
     'http://10.11.36.60:8080/#/signin',
     'http://10.11.36.61:8080/#/signin',
     'http://10.1.2.182/#/signin',
-    'https://gcurobot.quickconnect.cn'
+    'http://gcurobot.quickconnect.cn'
 ]
 
-let isRedirected = false
-for (let i = 0; i < testURL.length; i++) {
-    if (!isRedirected) {
+var testPromise = []
+var testStatus = new Array(testURL.length).fill(false)
+for (let i = 0; i != testURL.length; i++) {
+    testPromise.push(
         checkServer(1000, fetch(testURL[i], { mode: 'no-cors' }))
             .then((result) => {
-                isRedirected = true
-                console.log('Success: redirecting')
-                window.location.href = testURL[i]
+                testStatus[i] = true
+                console.log('Success: ' + testURL[i])
             })
             .catch((e) => {
                 console.log(e)
             })
-    }
+    )
 }
+
+Promise.all(testPromise)
+    .then(() => {
+        for (let i = 0; i != testURL.length; i++) {
+            if (testStatus[i]) {
+                window.location.href = testURL[i]
+                break
+            }
+        }
+    })
