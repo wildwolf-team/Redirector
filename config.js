baa5651778155b74577d031675d93deac610dc43
iChizer0 <62390647+iChizer0@users.noreply.github.com>
2021-09-11 16:06:00 +0800

    Refactor using service worker

---
+++ config.js
@@ -0,0 +1,8 @@
+var testURL = [
+    'http://10.11.36.60:8080/#/signin',
+    'http://10.11.36.61:8080/#/signin',
+    'http://10.1.2.182/#/signin',
+    'http://gcurobot.quickconnect.cn'
+]
+
+var testTimeout = 1000
