8e21c7fdaa23ecc71d5ffaeca954ad3687896b86
PaPr <36095960+PaPaPR@users.noreply.github.com>
2021-10-30 20:44:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ var testURL = [
     'http://10.11.36.60:8080/drive/#/signin',
     'http://10.11.36.61:8080/drive/#/signin',
     'http://10.1.2.182/drive/#/signin',
-    'http://gcurobot.quickconnect.cn'
+    'https://10-11-36-60.gcurobot.direct.quickconnect.to:5001/drive/#/signin'
 ]
 
 var testTimeout = 1000
