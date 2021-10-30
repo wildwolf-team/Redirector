98f8c7fd2b4c9cd93023a00bf1cabea61105b364
PaPr <36095960+PaPaPR@users.noreply.github.com>
2021-10-30 14:05:29 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var testURL = [
-    'http://10.11.36.60:8080/#/signin',
-    'http://10.11.36.61:8080/#/signin',
-    'http://10.1.2.182/#/signin',
+    'http://10.11.36.60:8080/drive/#/signin',
+    'http://10.11.36.61:8080/drive/#/signin',
+    'http://10.1.2.182/drive/#/signin',
     'http://gcurobot.quickconnect.cn'
 ]
 
