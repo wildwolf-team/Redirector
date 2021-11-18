28df9b5dd5b573dee20ff3ada18b853039690bfc
sweetdeath <36095960+PaPaPR@users.noreply.github.com>
2021-11-18 14:54:12 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ var testURL = [
     'http://10.11.36.60:8080/drive/#/signin',
     'http://10.11.36.61:8080/drive/#/signin',
     'http://10.1.2.182/drive/#/signin',
-    'https://10-11-36-60.gcurobot.direct.quickconnect.to:5001/drive/#/signin'
+    'https://gcurobot.cn2.quickconnect.cn/drive/#/signin'
 ]
 
 var testTimeout = 1000
